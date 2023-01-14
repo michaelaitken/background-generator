f40f17381baf4a8b1f3fb0a1db489e5e6f782ccb
Michael Aitken <mike@mikeaitken.net>
2023-01-14 13:37:07 +0800

    Reuploading correct files

--- script.js
+++ script.js
@@ -1,46 +1,18 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+var backgroundText = document.querySelector("h3");
+var leftColor = document.querySelector(".leftColor");
+var rightColor = document.querySelector(".rightColor");
+var body = document.getElementById("gradient");
 
-var listItems = document.querySelectorAll("li");
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ leftColor.value 
+	+ ", " 
+	+ rightColor.value 
+	+ ")";
 
-
-function inputLength() {
-	return input.value.length;
+	backgroundText.textContent = body.style.background + ";";
 }
 
-function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
-}
-
-function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
-}
-
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
-}
-
-// for (var i = 0; i < listItems.length; i++) {
-// 	var item = listItems[i]
-// 	item.addEventListener("click", function() {
-// 		item.classList.toggle("done");
-// 	})
-// };
-
-listItems.addEventListeners("click", function(){
-	listItems.classList.toggle("done")
-})
-
-button.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
-
-
+leftColor.addEventListener("input", setGradient);
+rightColor.addEventListener("input", setGradient);
