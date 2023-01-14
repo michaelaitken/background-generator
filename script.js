6dcc01f637278754fe09b6921b6f516babb2846f
Michael Aitken <mike@mikeaitken.net>
2023-01-14 14:36:44 +0800

    removed excess node modules

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var _ = require('lodash');
 
-console.log(_)
+var array = [1,2,3,4,5,6,7,8];
+console.log("Answer: ", _.without(array, 3))
 var backgroundText = document.querySelector("h3");
 var leftColor = document.querySelector(".leftColor");
 var rightColor = document.querySelector(".rightColor");
